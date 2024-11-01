2b295618b501ef3f3dbc581a97efb75a9ef13198
Nicolas Richard <he591969@ens.uqam.ca>
2024-11-01 18:44:26 -0400

    Adjusted Hive_cpp-java_files_extraction.py to reflect changes raw data

--- Hive_cpp-java_files_extraction.py
+++ Hive_cpp-java_files_extraction.py
@@ -5,10 +5,16 @@ import os
 repo_path = os.path.expanduser('~/Desktop/hive')
 os.chdir(repo_path)
 
+# Path to the 'bug_ids_with_versions.txt' file
+bug_ids_file_path = os.path.expanduser('~/Desktop/.MGL_869_Laboratoire/bug_ids_with_versions.txt')
+
+# Path to save the output CSV file
+output_file_path = os.path.expanduser('~/Desktop/.MGL_869_Laboratoire/modified_java_cpp_files.csv')
+
 # Load bug IDs and versions from the text file
 bug_file_map = {}
 
-with open('bug_ids_with_versions.txt', 'r') as f:
+with open(bug_ids_file_path, 'r') as f:
     for line in f:
         # Split each line by the comma to separate bug ID and version
         bug_id, version = line.strip().split(',')
@@ -40,7 +46,7 @@ for bug_id in bug_file_map:
         print(f"Error searching for {bug_id}: {e}")
 
 # Save modified files associated with bug IDs to a CSV file
-with open('modified_java_cpp_files.csv', 'w') as f:
+with open(output_file_path, 'w') as f:
     for bug_id, data in bug_file_map.items():
         # Write bug ID, version, and associated files
         f.write(f"{bug_id},{data['version']},")
